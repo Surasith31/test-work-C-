625ca11da5be736e31fceb33032131a23f83a510
Surasith31 <141249302+Surasith31@users.noreply.github.com>
2024-08-20 22:37:52 +0700

    commit

--- Point8/obj/Debug/net8.0/Point8.AssemblyInfo.cs
+++ Point8/obj/Debug/net8.0/Point8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Point8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82822e2582ba90db023e860aa1856431657e1e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Point8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Point8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
